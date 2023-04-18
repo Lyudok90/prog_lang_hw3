ab3404a64e01aecce35f0df9d8e66ead778dfc44
Lydok90 <lyudaharutyunyan@gmail.com>
2023-04-18 23:46:09 +0400

    3D

---
+++ 3D/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая принимает на вход координаты 
+двух точек и находит расстояние между ними в 3D пространстве.*/
+
+Console.WriteLine("Enter x1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+
+int X = x2 - x1;
+int Y = y2 - y1;
+int Z = z2 - z1;
+
+double length = X * X + Y * Y + Z * Z;
+Console.WriteLine("Length is " + length);
